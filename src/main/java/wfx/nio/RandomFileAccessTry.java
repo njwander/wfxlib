c1b2e9a7cbe44b65fd75eb7fc6b1742881927a89
njwander <wander1983@163.com>
2014-10-22 00:42:53 +0800

    增加了RandomFileAccess通过Channel和ByteBuffer进行读取的用例。

---
+++ src/main/java/wfx/nio/RandomFileAccessTry.java
@@ -0,0 +1,43 @@
+package wfx.nio;
+
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * Created by fanxin.wfx on 14-10-22.
+ */
+public class RandomFileAccessTry {
+    public void accessFile(String fileName) {
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile(fileName, "rw")) {
+            randomAccessFile.skipBytes(10);
+            FileChannel fileChannel = randomAccessFile.getChannel();
+            ByteBuffer byteBuffer = ByteBuffer.allocate(32);
+            int bytesRead = fileChannel.read(byteBuffer);
+            while (bytesRead != -1) {
+//                System.out.println("Read" + bytesRead);
+                //此处很重要
+                byteBuffer.flip();
+                while (byteBuffer.hasRemaining()) {
+                    char chr = (char)byteBuffer.get();
+                    if('\n' == chr){
+                        System.out.println("\n读完一行了");
+                        break;
+                    }
+                    System.out.print(chr);
+                }
+                byteBuffer.compact();
+                bytesRead = fileChannel.read(byteBuffer);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        RandomFileAccessTry randomFileAccessTry = new RandomFileAccessTry();
+        randomFileAccessTry.accessFile("src/main/resources/files/antx.properties");
+    }
+}
