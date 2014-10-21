ce443ecf603a6246621d7670b9eb27faa17b982a
njwander <wander1983@163.com>
2014-10-22 00:49:47 +0800

    转换了UTF-8编码

--- src/main/java/wfx/nio/RandomFileAccessTry.java
+++ src/main/java/wfx/nio/RandomFileAccessTry.java
@@ -24,6 +24,7 @@ public class RandomFileAccessTry {
                     char chr = (char)byteBuffer.get();
                     if('\n' == chr){
                         System.out.println("\n读完一行了");
+
                         break;
                     }
                     System.out.print(chr);
